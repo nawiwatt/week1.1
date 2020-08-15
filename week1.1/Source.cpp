7f9aa0333a61a0e408db13d6aadab3ae0caa4cce
phuph <phuph@MSI>
2020-08-15 15:12:28 +0700

    Add project files.

---
+++ week1.1/Source.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int m = 999999999;
+	int M = -999999999;
+	int a, n, i;
+	scanf("%d", &n);
+	for (i = 0;i < n;i++)
+	{
+		scanf("%d", &a);
+		if (a < m)
+		{
+			m = a;
+		}
+		if (a > M)
+		{
+			M = a;
+		}
+	}
+	printf("%d\n%d", m, M);
+}
